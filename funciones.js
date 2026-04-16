e9535d243bad2a4069b479f1bb72ee99ccf70da0
Norfolk <alexander.garofalo.ag@gmail.com>
2026-04-15 21:52:34 -0500

    T2.P1.2-5 Avance taller en clase

--- funciones.js
+++ funciones.js
@@ -1,1 +1,21 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+//Paso 2 - Calcular Disponible
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        disponible = 0;
+    }
+    return disponible;
+}
+
+//Paso 4 - Función calcularCapacidadPago 
+function calcularCapacidadDePago(montoDisponible){
+  let capacidadDePago = montoDisponible / 2 ;
+  return capacidadDePago;
+}
+
+function texto(id,var1){
+  let texto1 = document.getElementById(id);
+  texto1.innerText = var1.toFixed(2);
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+ 
+ // Paso 3 - Funcion Calcular
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+ 
+    let disponible = calcularDisponible(ingresos, egresos);
+    const elemento = document.getElementById("spnDisponible");
+    elemento.innerText = disponible;
+    texto("spnDisponible", disponible);
+
+    let capacidadPago = calcularCapacidadPago(disponible);
+    const elemento2 = document.getElementById("spnCapacidadPago");
+    elemento2.innerText = capacidadPago;
+    texto("spnCapacidadPago", capacidadPago);
+}
